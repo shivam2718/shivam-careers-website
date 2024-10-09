1fc5ef39def88f34d324fbd6bc37918a95d73696
shivam2718 <shivamkumar3.14proffesional@gmail.com>
2024-10-09 16:20:13 +0000

    added requirements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
@@ -32,7 +32,9 @@ JOBS=[
 @app.route("/")
 def hello_jovian():
   return render_template('home.html',jobs=JOBS,company_name='shivam')
-
+@app.route("/api/jobs")
+def list_jobs():
+ return jsonify(JOBS)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
         
